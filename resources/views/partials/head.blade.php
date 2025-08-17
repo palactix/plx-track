86d210a9b308478e3d8002c57bdff1745df3619f
Jitendra Meena <19606386+jitu-git@users.noreply.github.com>
2025-08-17 17:44:45 +0530

    fav incons

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,10 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/fav-icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/fav-icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/fav-icons/favicon-16x16.png">
+<link rel="manifest" href="/fav-icons/site.webmanifest">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
